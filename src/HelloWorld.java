2c0f56eee4173be8910eaa3084402f2ec52e6c23
AdmiN <AdmiN@ITWS0030>
2024-08-27 11:27:55 +0530

    Committed at 11:27

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,10 @@
-
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hellos");
-		System.out.println("Hola.....");
+		System.out.println("Hola..... Welcome to DevOps...");
+		System.out.println("Hola....New to DevOps");
 	}
 
 }
--- src/NextModeule.java
+++ src/NextModeule.java
@@ -1,4 +1,3 @@
-
 public class NextModeule {
 
 	public static void main(String[] args) {
